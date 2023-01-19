b8dd65c5394bcc6413352b2cbfdc6dce771e9cb5
Nicholas Schrimpf <nickschrimpf@users.noreply.github.com>
2023-01-19 07:01:50 -0700

    Jan19

---
+++ Jan 2023/Jan192023.js
@@ -0,0 +1,14 @@
+// 8Kyu
+// You will be given an array a and a value x.
+// All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+function check(a, x) {
+    return a.includes(x);
+}
+
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+// Give your answer as a string matching "odd" or "even".
+// If the input array is empty consider it as: [0] (array with a zero).
+
