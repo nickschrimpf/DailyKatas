6241796f895c13739d047103662fbf2a69e1d3d0
Nicholas Schrimpf <nickschrimpf@users.noreply.github.com>
2023-02-06 08:31:39 -0700

    another one in the books

--- Feb2023/feb6.js
+++ Feb2023/feb6.js
@@ -1,4 +1,20 @@
+// 8KYU
+
 function squareSum(numbers){
-numbers.map((e)=>Math.pow(e,2)).reduce((a,b)=>a+b)
+    return numbers.map((e)=>e*e).reduce((a,b)=>a+b)
 }
-squareSum([1,2])
+console.log(squareSum([1,2]))
+
+// 8KYU
+function fakeBin(x){
+    let result = "";
+    for(let i = 0; i < x.length; i++){
+      if(x[i] >= '5'){
+        result += '1';
+      }else{
+        result += '0';
+      }
+    }
+    return result;
+  }
+console.log(fakeBin('4538559312789'))
